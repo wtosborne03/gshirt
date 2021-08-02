05a8d129cf287dee9ef04f5b57978306e40a90aa
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2021-08-02 08:27:42 -0400

    first commit

---
+++ rgoog/script.js
@@ -0,0 +1,51 @@
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+var oposition;
+var eposition;
+
+function start() {
+    const soundEffect = new Audio('sound.wav');
+    soundEffect.play();
+    navigator.geolocation.getCurrentPosition(setupmap, error, options);
+    
+}
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+function setupmap(position) {
+    oposition = position.coords;
+    $('.main').html('<div class="overtext container mw-100"><div class="row"><div class="col btext">Choose Location</div><div class="col btext"><button id="go" class="button-n" onclick="go()">GO</button></div></div></div><div id="map"></div>');
+    $('#go').hide();
+    map();
+}
+
+function map() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoid3Rvc2Jvcm5lMDMiLCJhIjoiY2tna3F6ODF2MDdsMTJzbG4yNWs3dWJxaiJ9.902DiSI6jaX-O_f_seKSiQ';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-79.4512, 43.6568],
+        zoom: 13
+    });
+    geo = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+        
+    });
+    geo.on('result', function(results) {
+        console.log(results);
+        eposition = results[0].geometry.coordinates;
+        $('#go').show();
+     })
+    map.addControl(geo);
+}
+function go() {
+    rURL = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' + oposition.latitude + ',' + oposition.longitude + '&destinations=' + eposition[0] + ',' + eposition[1] + '&key=YOUR_API_KEY';
+    fetch(rURL)
+    .then(response => response.json())
+    .then(data => console.log(data));
+    $('.main').html();
+}
